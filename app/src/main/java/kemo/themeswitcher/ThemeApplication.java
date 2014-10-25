897bbe3cfacb54cc0fb06f7834cd90658c35d5d0
Kevin Mo <cassinomo@yahoo.com>
2014-10-24 20:39:05 -0700

    add theme

---
+++ app/src/main/java/kemo/themeswitcher/ThemeApplication.java
@@ -0,0 +1,11 @@
+package kemo.themeswitcher;
+
+import android.app.Application;
+
+/**
+ * Created by kemo on 10/21/14.
+ */
+public class ThemeApplication extends Application {
+    // App level variable to retain selected spinner value
+    public static int currentPosition;
+}
---
+++ app/src/main/java/kemo/themeswitcher/ThemeSwitchActivity.java
@@ -0,0 +1,47 @@
+package kemo.themeswitcher;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+
+
+public class ThemeSwitchActivity extends Activity {
+
+    private Spinner spThemes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Utils.onActivityCreateSetTheme(this);
+        setContentView(R.layout.activity_theme_switch);
+        setupSpinnerItemSelection();
+    }
+
+    private void setupSpinnerItemSelection() {
+        spThemes = (Spinner) findViewById(R.id.spThemes);
+        spThemes.setSelection(ThemeApplication.currentPosition);
+        ThemeApplication.currentPosition = spThemes.getSelectedItemPosition();
+
+        spThemes.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                if (ThemeApplication.currentPosition != position) {
+                    Utils.changeToTheme(ThemeSwitchActivity.this, position);
+                }
+                ThemeApplication.currentPosition = position;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+}
